0dd666f430f9248eb454f078142ff876ad8637a7
Kim Yu Ng <kimyu_92@hotmail.com>
2015-07-16 18:13:44 -0500

    iframe work ggwp

--- test.rb
+++ test.rb
@@ -58,28 +58,36 @@ def a_single_tour
     count += 1
   end
 
+
   # Search the last keyword in array
   search_input.send_keys(:enter) 
   sleep(3)
+
 
   # Make sure media content loaded
   search_input.send_keys(:arrow_down)
   search_input.send_keys(:arrow_down)
 
+
   # Click image for prank
-  sleep(5)
-  first_media_element = driver.find_element(:css => '.media-0')
+  sleep(1)
+  grab_firs_media_element = Proc.new {driver.find_elements(:css => '.media-0')}
+  first_media_elements = retry_method(&grab_firs_media_element)
+  first_media_element = first_media_elements[0]
   first_media_element.click()
+
 
   # Wait for modal to populate
   sleep(3)
   media_modal = driver.find_element(:css => '.modal-content')
   media_modal.click()
 
+
   # Close modal
   sleep(2)
   cancel_modal = driver.find_element(:css => '.close')
   cancel_modal.click()
+
 
   # Export to png
   # Click the export modal
@@ -87,10 +95,12 @@ def a_single_tour
   export_modal = driver.find_element(:css => '.export-dropdown')
   export_modal.click()
 
+
   # Click Export as PNG
   sleep(2)
   export_as_png = driver.find_elements(:css => '.export')[2]
   export_as_png.click()
+
 
   # Choose comprehensive view 
   sleep(2)
@@ -98,19 +108,46 @@ def a_single_tour
   export_png_as_comprehensive[0].click()
   sleep(0.5)
 
+
   # Export now and wait for cloud modal
   export_png_now = driver.find_element(:css => '.modal-footer .btn')
   export_png_now.click()
-  binding.pry
+
 
   # Wait until it finish processing then cloud modal will pop out
-  # filepicker_modal = driver.find_element(:css => '.fp__header__title ng-binding')
+  grab_filepicker_modal = Proc.new { driver.find_elements(:xpath, "//iframe[contains(@id, 'filepicker_dialog')]") }
+  filepicker_modal = retry_method(&grab_filepicker_modal)
+  filepicker_modal = filepicker_modal[0]
+  filepicker_modal.click() # sanity check iframe exists
+
+
+  # Switch to iframe and click Box Cloud
+  driver.switch_to.frame(filepicker_modal)
+  driver.find_elements(:css => ".sbicon-box")[0].click()
+  puts "hooray llll"
+  binding.pry
 
 end
+
+# Retry till the element exist
+def retry_method(&block)
+  ele_arr = nil
+  while true do
+    ele_arr = block.call
+    break if ele_arr.length > 0
+    sleep(1)
+    puts 'checking >>>'
+  end 
+  ele_arr
+end
+
 
 a_single_tour()
 
 # End of driver
-# sleep(15)
-# driver.close()
+
+
+# useful iframe handling
+# driver.switch_to.default_content()
+# driver.switch_to.frame(0) # can feed in id, object
 
